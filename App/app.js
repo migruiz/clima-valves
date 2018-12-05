dd5e5a963e9577921f15f241709596ec6f4a4212
miguel <mig.ruiz@gmail.com>
2018-12-05 17:00:38 +0000

    teting new valve

--- App/app.js
+++ App/app.js
@@ -2,95 +2,46 @@ var mqtt = require('./mqttCluster.js');
 const BoilerValve=require('./BoilerValve')
 global.config = {
     zwaveDriverPath: '/dev/ttyACM0',
-    dbPath: 'c:\\valves.sqlite',
-    intranetAMQPURI: process.env.TEMPQUEUEURL,
-    internetAMQPURI: 'amqp://ikuaqslf:B6blp8fWqyBE4Ya7NBXhhVEk1guTw-M8@impala.rmq.cloudamqp.com/ikuaqslf',
-    valves: {
-        upstairsValve: { nodeId: 5, instanceId: 1, code: 'upstairsValve' },
-        downstairsValve: { nodeId: 5, instanceId: 3, code: 'downstairsValve' },
-        testValve: { nodeId: 4, instanceId: 1, code: 'testValve' },
-        hotWaterValve: { nodeId: 4, instanceId: 3, code: 'hotWaterValve' }
-    }
+    valves: [
+        new Valve({ nodeId: 5, instanceId: 1, code: 'upstairsValve' }),
+        new Valve({ nodeId: 5, instanceId: 3, code: 'downstairsValve'}),
+        new Valve({ nodeId: 4, instanceId: 1, code: 'testValve' }),
+        new Valve({ nodeId: 4, instanceId: 3, code: 'hotWaterValve' })
+    ]
 };
-global.zonesConfiguration= {
-    masterroom: 'upstairs',   
-    livingroom:  'downstairs',
-    playroom:  'upstairs', 
-    masterbathroom:  'upstairs',
-    computerroom:  'upstairs',
-    secondbedroom: 'upstairs'
-}
-global.boilerValves={
-    upstairs:new BoilerValve('upstairs'),
-    downstairs:new BoilerValve('downstairs')
-}
 
-
-var ZWaveMockMan = require('./ZWaveMock.js');
 
 global.mtqqLocalPath = "mqtt://localhost";
-(async function(){
-    for (var key in global.zonesConfiguration) {
-        var zoneConfig=global.zonesConfiguration[key]
-        var boilerValve=global.boilerValves[zoneConfig]
-        boilerValve.addZone(key) 
-    }
-    await global.boilerValves.upstairs.initAsync();
-    await global.boilerValves.downstairs.initAsync();
-    var mqttCluster=await mqtt.getClusterAsync() 
-    subscribeToEvents(mqttCluster)
-
-  })();
-
-  function subscribeToEvents(mqttCluster){
-    mqttCluster.subscribeData('automaticboilercontrol/upstairs',function(content) {
-        console.log('automaticboilercontrol/upstairs')
-        console.log(content)
-    })
-    mqttCluster.subscribeData('automaticboilercontrol/downstairs',function(content) {
-        console.log('automaticboilercontrol/downstairs')
-        console.log(content)
-    })
-  }
-
-return;
 
 
-//var ZWave = require('./node_modules/openzwave-shared/lib/openzwave-shared.js');
+var ZWave = require('./node_modules/openzwave-shared/lib/openzwave-shared.js');
 var os = require('os');
-var individualValvesManagerCreator = require('./individualValveManager.js');
-var zonesValvesManagerCreator = require('./ZonesManager/ZonesValvesManager.js');
-var individualValveOperationReceiver = require('./individualValveOperationReceiver.js');
-var znodeValuesListener = require('./ValvesValueChangesListener.js');
-var valvesStateRequestReceiver = require('./valvesStateRequestReceiver.js');
-var zonesValvesConfigurationReceiver = require('./zonesValvesConfigurationReceiver.js');
-var firebaseSyncReceiver = require('./firebaseSyncReceiver.js');
 
 
 
 
+var zwave = new ZWave({ ConsoleOutput: false });
 
 
+zwave.on('scan complete', async function () {
+    for (let index = 0; index < this.modules.length; index++) {
+        var valve=global.config.valves[index];
+        await valve.initAsync(); 
+    }
+    zwave.on('value changed',async function (nodeid, comclass, value){
+        var valveReading = {
+            nodeId: nodeid,
+            instanceId: value.instance,
+            value: value.value,
+            commandType: value.class_id,
+            timestamp: Math.floor(new Date() / 1000)
+        };
+        for (let index = 0; index < this.modules.length; index++) {
+            var valve=global.config.valves[index];
+            await valve.handleValveStateChangeAsync(valveReading);    
+        }
 
-//var zwave = new ZWave({ ConsoleOutput: false });
-var zwave = new ZWaveMockMan.ZWaveMock();
-
-
-zwave.on('scan complete', function () {
-    var individualValveManager = individualValvesManagerCreator.getInstance(zwave);
-
-    var zonesValvesManager = zonesValvesManagerCreator.getInstance(individualValveManager);
-
-    individualValveOperationReceiver.startMonitoring(individualValveManager);
-    valvesStateRequestReceiver.startMonitoring(individualValveManager);
-
-    var valvesValueChangesListener = znodeValuesListener.getInstance(individualValveManager);
-    zwave.on('value changed', valvesValueChangesListener.handleChange);
-
-    firebaseSyncReceiver.startMonitoring();
-
-    zonesValvesConfigurationReceiver.startMonitoring(zonesValvesManager);
-    zonesValvesManager.startMonitoring();
+    });
 });
 
 
